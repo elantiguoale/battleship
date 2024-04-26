f6b0609387c951244323207c5e87c4db91e8a534
Miguel Hernandez <miguelalejandroh@hotmail.com>
2024-04-26 14:56:43 +0000

    created the base functions for the future project

--- run.py
+++ run.py
@@ -1,7 +1,23 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
 
+OUR_BOARD = [[" "] * 8 for x in range(8)]
+COMPUTER_BOARD = [[" "] * 8 for x in range(8)]
 
-print("hello world")
-print("hello world two")
+letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F':5, 'G':6, 'H': 7 }
+
+def board(board):
+    print('     A B C D E F G H')
+    print("     ---------------")
+    row_number = 1
+    for row in board:
+        print("%d|%s|" % (row_number, "|".join(row)))
+        row_number += 1
+
+def create_ships():
+    pass
+
+def get_ship_location():
+    pass
+
+def count_hit_ships():
+    pass
